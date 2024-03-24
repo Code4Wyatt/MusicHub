73503a2713180b05752174aaf7fd648dd742a2bc
Code4Wyatt <44061707+Code4Wyatt@users.noreply.github.com>
2024-03-24 18:51:43 +0000

    complete sanity setup

--- musichub/app/lib/sanity.ts
+++ musichub/app/lib/sanity.ts
@@ -1,3 +1,4 @@
+import imageUrlBuilder from "@sanity/image-url";
 import { createClient } from "next-sanity";
 
 export const client = createClient({
@@ -6,3 +7,9 @@ export const client = createClient({
     apiVersion: "22-03-25",
     useCdn: true,
 });
+
+const builder = imageUrlBuilder(client);
+
+export function urlFor(source: any) {
+    return builder.image(source);
+}
