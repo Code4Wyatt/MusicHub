eceae144f4d7d8aba49050efc0c92f59a349f24a
Code4Wyatt <44061707+Code4Wyatt@users.noreply.github.com>
2024-03-24 20:42:35 +0000

    add hero section

--- musichub/app/lib/sanity.ts
+++ musichub/app/lib/sanity.ts
@@ -4,7 +4,7 @@ import { createClient } from "next-sanity";
 export const client = createClient({
     projectId: "gwehfp8q",
     dataset: "production",
-    apiVersion: "22-03-25",
+    apiVersion: "2022-03-25",
     useCdn: true,
 });
 
